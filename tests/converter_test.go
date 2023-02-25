393d5a7725cbfcf6931143c92e5ba31ccf5e6161
Zaur <fameless.4ell@gmail.com>
2023-02-25 15:19:23 +0500

    tests: add converter_test.go

---
+++ tests/converter_test.go
@@ -0,0 +1,34 @@
+package test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/Fameless4ellL/cli-file-converter/converter"
+)
+
+func TestConvertCSVToJSON(t *testing.T) {
+    // Create a test CSV file
+    csvfile, err := os.Create("test.csv")
+    if err != nil {
+        t.Errorf("Error creating test CSV file: %s", err)
+    }
+    defer csvfile.Close()
+
+    // Write some test data to the CSV file
+    csvfile.WriteString("name,age,city\n")
+    csvfile.WriteString("John,30,New York\n")
+    csvfile.WriteString("Mary,25,Chicago\n")
+    csvfile.WriteString("Peter,35,Los Angeles")
+
+    // Convert the test CSV file to JSON
+    err = converter.ConvertCSVToJSON("test.csv", "test.json", ",")
+    if err != nil {
+        t.Errorf("Error converting CSV to JSON: %s", err)
+    }
+
+    // Verify the contents of the JSON file
+    if _, err := os.Stat("test.json"); os.IsNotExist(err) {
+        t.Errorf("Error converting CSV to JSON: %s", err)
+    }
+}
